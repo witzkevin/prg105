c7ed07d2ef6bf778ec944615122a307bcbdd53ca
witzkevin <kwitz954@students.mchenry.edu>
2017-02-08 23:19:41 -0600

    Add files via upload

---
+++ if statements.py
@@ -0,0 +1,25 @@
+x = int(input(" Pick a number 1-7 "))
+
+if x == 1:
+    print("monday")
+
+if x == 2:
+    print("tuesday")
+
+if x == 3:
+    print("wednesday")
+
+if x == 4:
+    print("thursday")
+
+if x == 5:
+    print("friday")
+
+if x == 6:
+    print("saturday")
+
+if x == 7:
+    print("sunday")
+
+if x >= 8 or x < 1:
+    print(" error message, please pick a number 1-7 ")
