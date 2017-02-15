2298bd9cbba73d49bc6a7b70819e99f8bef622d9
witzkevin <kwitz954@students.mchenry.edu>
2017-02-15 09:43:01 -0600

    Add files via upload

---
+++ cals burned.py
@@ -0,0 +1,12 @@
+caloriesBurned = 4.2
+
+for time in range(10, 31, 5):
+    numberOfCalories = (time * caloriesBurned)
+    print(" You will burn ", numberOfCalories, " calories in", time, "minutes")
+
+
+
+
+
+
+
