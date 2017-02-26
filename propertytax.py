14216db19f5432c650dfbb38a8c2e511ce41c74c
witzkevin <kwitz954@students.mchenry.edu>
2017-02-26 15:51:31 -0600

    Update propertytax.py

--- propertytax.py
+++ propertytax.py
@@ -1,11 +1,11 @@
-def get_value():
+def get_value(): # Asked user to for value of home and set a variable up
     value = float(input("What is the actual value of your property? "))
     calculate(value)
 
 
-def calculate(my_value):
+def calculate(my_value): # Here is where the math calculations take place.
     print("The county assessment value is: $", format((my_value * .60), ",.2f"))
     assessment_value = my_value * .6
     print("The final tax for the acre assessment is: $ ", format((assessment_value * .0072), ",.2f"))
 
-get_value()
+get_value() # running the final program
