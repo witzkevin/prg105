0cc1065757c3d1084d3bee9406b4968720396a6f
witzkevin <kwitz954@students.mchenry.edu>
2017-03-19 18:02:30 -0500

    Add files via upload

---
+++ 8.1name.py
@@ -0,0 +1,9 @@
+def main():
+    # Getting my variables for the name to take first letter in each
+    first_name = input("Enter your first name: ")
+    middle_name = input("Enter your middle name: ")
+    last_name = input("Enter your last name: ")
+    print(first_name[0], ".", middle_name[0], ".", last_name[0], ".")
+
+# Running the code
+main()
