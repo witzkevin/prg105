93ad09c7343686c8302ca2260f5b1e460ddf6acc
witzkevin <kwitz954@students.mchenry.edu>
2017-04-12 19:58:10 -0500

    Add files via upload

---
+++ 10.1/10.1_2.py
@@ -0,0 +1,39 @@
+class Personal:
+    # The inint method initializes the attributes
+    def main(self)  :
+        def __init__(self, personal, friend_1, friend_2):
+            self.__personal = personal
+            self.__friend_1 = friend_1
+            self.__friend_2 = friend_2
+
+        # Def set accepts an argument
+        def set_personal(self, personal):
+            self.__personal = personal
+
+        # Def set accepts an argument
+
+        def set_friend_1(self, friend_1):
+            self.__friend_1 = friend_1
+
+        # Def set accepts an argument
+
+        def set_friend_2(self, friend_2):
+            self.__friend_2 = friend_2
+
+        # Def get method returns  the statement
+        def get_personal(self):
+            personal = input("Enter my your name and phone number:")
+            print(personal)
+            return self.__personal
+
+
+        def get_friend_1(self):
+            friend_1 = input("Enter my your name and phone number for friend 1:")
+            return self.__friend_1
+
+        def get_friend_2(self):
+            friend_2 = input("Enter my your name and phone number for friend 2:")
+            return self.__friend_2
+
+
+    main()
